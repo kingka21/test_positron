8c5ae197ef874eb46fedc8bf0304e893f9a1987e
GitHub_userName <GitHub_userEmail>
2026-06-26 10:42:03 -0400

    first commit

---
+++ test_scripts.R
@@ -0,0 +1,10 @@
+library(dplyr)
+head(cars)
+
+# Summary statistics
+summary(cars)
+
+# Simple plot
+plot(cars$speed, cars$dist, main = "Cars Dataset",
+     xlab = "Speed (mph)", ylab = "Stopping Distance (ft)",
+     pch = 19, col = "blue")
